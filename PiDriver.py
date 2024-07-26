5f7c089dd6f0da37e8d6301a080112baa7e23b6d
Parrot Live user <user@localhost.localdomain>
2024-07-26 23:06:31 +0000

    Added: entry_point

---
+++ PiDriver.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+# This script runs PiDriver from within a cloned git repo.
+# The script 'bin/pidriver' is designed to be run after installation. (/usr/sbin/pidriver)
+
+from pidriver import __main__
+__main__.entry_point()
