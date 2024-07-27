203a694703504ffff3a6f51549375dbcbc2c692c
Parrot Live user <user@localhost.localdomain>
2024-07-27 17:48:44 +0000

    Deleted Files

--- PiDriver.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-
-# This script runs PiDriver from within a cloned git repo.
-
-from pidriver import __main__
-__main__.entry_point()
--- pidriver/__main__.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python
-
-def entry_point():
-        print("Hello World")
