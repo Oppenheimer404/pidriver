d9faa0eec8922f12fe2408ef33ef7e2e8a00bbf0
Parrot Live user <user@localhost.localdomain>
2024-07-27 16:37:30 +0000

    First attempt at setup.py

--- setyp.py
+++ setyp.py
@@ -1,2 +1,22 @@
 from distutils.core import setup
 
+setup(
+    name='pidriver',
+    version='0.0.1',
+    author='Oppenheimer404',
+    url='https://github.com/Oppenheimer404/PiDriver',
+    packages=[
+        'pidriver',
+        'pidriver/database',
+        'pidriver/gps',
+        'pidriver/network',
+        'pidriver/reporter',
+        'pidriver/scanner',
+    ],
+    entry_points={
+        'console_scripts': [
+            'wifite = wifite.wifite:entry_point'
+        ]
+    },
+    scripts=['bin/pidriver'],
+)
