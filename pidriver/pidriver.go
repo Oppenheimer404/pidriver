09c08fa4bef53247e32d8db380aeb2793e5466fa
Oppenheimer404 <oppenheimer404@proton.me>
2024-09-29 23:18:26 -0400

    Basic scan call working

--- pidriver/pidriver.go
+++ pidriver/pidriver.go
@@ -1,21 +1,44 @@
 package main
 
 import (
-    // "fmt"
-    "log"
-    "github.com/oppenheimer404/pidriver/pidriver/scanner"
+	"fmt"
+	"log"
+    "flag"
+	// "time"
+	// "github.com/oppenheimer404/pidriver/pidriver/logger"
+	"github.com/oppenheimer404/pidriver/pidriver/scanner"
+	// "github.com/oppenheimer404/pidriver/pidriver/gps"
 )
 
+// Constant Variables
 const (
+    // scan requests
     REQUEST_WIFI_DATA = "wifi"
     REQUEST_BLUETOOTH_DATA = "bt"
+    // logging setup
+    FILETYPE_CSV = "csv"
 )
 
 func main() {
-    response, err := scanner.Request(REQUEST_WIFI_DATA)
+    // Ticker for testing purposes
+    // ticker := time.NewTicker(1 * time.Second)
+    // toggle := false
+    // for range ticker.C {
+    //     toggle = !toggle
+    //     var data string
+    //     if toggle {
+    //         data = request(REQUEST_WIFI_DATA)
+    //     } else if !toggle {
+    //         data = request(REQUEST_BLUETOOTH_DATA)
+    //     }
+    //     fmt.Println(data)
+    // }
+}
+
+func requestScan(requestType string) string {
+    response, err := scanner.Request(requestType) // Collect data from scanner
     if err != nil {
-        log.Println(err)
-    } else {
-        log.Println(response)
+        log.Fatal(err) // Fatal error if requestType is invalid
     }
+    return response
 }
