55b8dd5a4b2f5b0605db0c5c8938233f7cac2b68
Oppenheimer404 <oppenheimer404@proton.me>
2024-10-11 00:39:08 -0400

    Configuration Working

--- pidriver/config/config.go
+++ pidriver/config/config.go
@@ -6,44 +6,83 @@ import (
 	"os"
 )
 
-// RequestUpdateConfig defines constants for updating configuration fields.
-type RequestUpdateConfig int
-
 const (
-	APP_NAME RequestUpdateConfig = iota
-	VERSION
-	BANNER
+	DEFAULT_CONF = "config.json"
 )
 
-// Config represents the configuration structure.
-type Config struct {
-	AppName string `json:"appName"`
-	Version string `json:"version"`
-	Author string `json:"author"`
-	Banner  string `json:"banner"`
+// Config represents a dynamic configuration map
+type Config map[string]interface{}
+
+// Default configuration values
+var defaultConfig = Config{
+	"AppName": "pidriver",
+	"Author":  "Oppenheimer404",
+	"Banner": `
+	
+	@@@@@@@  @@@ @@@@@@@  @@@@@@@  @@@ @@@  @@@ @@@@@@@@ @@@@@@@  
+	@@!  @@@ @@! @@!  @@@ @@!  @@@ @@! @@!  @@@ @@!      @@!  @@@ 
+	@!@@!@!  !!@ @!@  !@! @!@!!@!  !!@ @!@  !@! @!!!:!   @!@!!@!  
+	!!:      !!: !!:  !!! !!: :!!  !!:  !: .:!  !!:      !!: :!!  
+	:       :   :: :  :   : : :      ::    : :: ::   :   : : 
+	
+	`,
+	"Version": "0.0.1",
 }
 
-// Default values for the configuration.
-var defaultConfig = Config{
-	AppName: "pidriver",
-	Version: "0.0.1",
-	Author: "Oppenheimer404",
-	Banner: `
-                                                              
-@@@@@@@  @@@ @@@@@@@  @@@@@@@  @@@ @@@  @@@ @@@@@@@@ @@@@@@@  
-@@!  @@@ @@! @@!  @@@ @@!  @@@ @@! @@!  @@@ @@!      @@!  @@@ 
-@!@@!@!  !!@ @!@  !@! @!@!!@!  !!@ @!@  !@! @!!!:!   @!@!!@!  
-!!:      !!: !!:  !!! !!: :!!  !!:  !: .:!  !!:      !!: :!!  
- :       :   :: :  :   :   : : :      ::    : :: ::   :   : : 
-                                                              
-`,
+// createDefaultConfig initializes the default configuration and writes it to a file.
+func createDefaultConfig(filePath string) (*Config, error) {
+	fmt.Println("Creating new configuration file with default values...")
+	
+	if err := writeConfigToFile(&defaultConfig, filePath); err != nil {
+		return nil, err
+	}
+	
+	return &defaultConfig, nil
+}
+
+// writeConfigToFile writes the configuration to a specified file.
+func writeConfigToFile(config *Config, filePath string) error {
+	file, err := os.Create(filePath)
+	if err != nil {
+		return fmt.Errorf("failed to open config file %s for writing: %v", filePath, err)
+	}
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	encoder.SetIndent("", "    ") // Pretty print
+	if err := encoder.Encode(config); err != nil {
+		return fmt.Errorf("failed to save config to file %s: %v", filePath, err)
+	}
+	
+	return nil
+}
+
+// Update dynamically updates a key in the configuration map.
+func (c *Config) Update(key string, value interface{}) error {
+	(*c)[key] = value
+	return writeConfigToFile(c, DEFAULT_CONF)
+}
+
+// Reset restores the configuration to its default values.
+func (c *Config) Reset() error {
+	*c = defaultConfig
+	return writeConfigToFile(c, DEFAULT_CONF)
+}
+
+// List lists the current configuration
+func (c *Config) List() {
+    fmt.Println("Current Configuration:")
+    for key, value := range *c {
+        fmt.Printf("%s: %v\n", key, value)
+    }
 }
 
 // Load loads the configuration from config.json, or creates it if not found.
 func Load() (*Config, error) {
-	file, err := os.Open("config.json")
+	filePath := DEFAULT_CONF
+	file, err := os.Open(filePath)
 	if os.IsNotExist(err) {
-		return createDefaultConfig()
+		return createDefaultConfig(filePath)
 	} else if err != nil {
 		return nil, fmt.Errorf("failed to open config file: %v", err)
 	}
@@ -55,53 +94,4 @@ func Load() (*Config, error) {
 	}
 
 	return &config, nil
-}
-
-// createDefaultConfig initializes the default configuration and writes it to a file.
-func createDefaultConfig() (*Config, error) {
-	fmt.Println("Config file not found, creating a new one...")
-
-	if err := writeConfigToFile(&defaultConfig); err != nil {
-		return nil, err
-	}
-
-	return &defaultConfig, nil
-}
-
-// Update changes a specified value in the configuration.
-func (c *Config) Update(key RequestUpdateConfig, value string) error {
-	switch key {
-	case APP_NAME:
-		c.AppName = value
-	case VERSION:
-		c.Version = value
-	case BANNER:
-		c.Banner = value
-	default:
-		return fmt.Errorf("invalid key: %d", key)
-	}
-
-	return writeConfigToFile(c)
-}
-
-// Reset restores the configuration to its default values.
-func (c *Config) Reset() error {
-	fmt.Println("Resetting to default config...")
-	return writeConfigToFile(&defaultConfig)
-}
-
-// writeConfigToFile writes the provided configuration to the config.json file.
-func writeConfigToFile(config *Config) error {
-	file, err := os.Create("config.json")
-	if err != nil {
-		return fmt.Errorf("failed to open config file for writing: %v", err)
-	}
-	defer file.Close()
-
-	encoder := json.NewEncoder(file)
-	if err := encoder.Encode(config); err != nil {
-		return fmt.Errorf("failed to save config: %v", err)
-	}
-
-	return nil
 }
